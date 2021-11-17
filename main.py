b88fc08ff5eff1edfdfef8c3c60abbef85214932
d3m0-sm <d3m0_sm@posteo.de>
2021-11-18 00:28:22 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+from os import popen as cmd
+import platform
+import sys
+
+grep = "grep"
+plat = platform.system()
+if plat == "Windows":
+    grep = "findstr"
+try:
+   cmd("adb devices")
+except:
+    print("Fatal Error: ADB is not installed")
+    sys.exit()
+data_path = ""
+app = "9"
+ac = cmd("adb devices")
+devices = ac.read().replace("List of devices attached", "")
+if "device" not in devices:
+    print("No device connected!")
+    sys.exit()
+else:
+    print("Device found\n")
+while app not in data_path:
+    app = input("Please specify which application do you want to dump: ")
+    pm = cmd(f"adb shell pm list packages -f | {grep} {app}")
+    data_path = pm.read().replace("package:", "").split(".apk")
+    data_path = data_path[0] + ".apk"
+    if app not in data_path:
+        print("App not found try again")
+        print("Hint: maybe try the publisher or developer\n")
+result_path = f"C:\\Users\\%username%\\Downloads\\{app}.apk"
+cmd(f"adb pull {data_path} {result_path}")
+print("APK successfully stored in Downloads directory")
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='APKGrabber',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='GPL',
+    author='Démo',
+    author_email='',
+    description=''
+)
